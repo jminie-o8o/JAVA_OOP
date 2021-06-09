b280343eddf68e72b68435e5139f596cd7b06fd3
jminie <jminie@naver.com>
2021-06-09 20:40:03 +0900

    JAVA_OOP_3

--- MyOOP.java
+++ MyOOP.java
@@ -5,26 +5,19 @@ public class MyOOP {
 
 	public static void main(String[] args) {
 		
-		Print p1 = new Print();
-		p1.delimiter = "****";
+		Print p1 = new Print("----");
 		p1.A();
 		p1.A();
 		p1.B();
 		p1.B();
 		
-		Print p2 = new Print();
-		p2.delimiter = "----";
+		Print p2 = new Print("****");
 		p2.A();
 		p2.A();
 		p2.B();
 		p2.B();
 		
-		Print p3 = new Print();
-		p3.delimiter = "^^^^";
-		p3.A();
-		p3.A();
-		p3.B();
-		p3.B();
+
 		
 		
 
