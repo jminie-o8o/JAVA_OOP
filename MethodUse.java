b890740b9d426e496ecea2a292894c7a1c005550
jminie <jminie@naver.com>
2021-06-27 17:26:37 +0900

    JAVA_OOP-5

---
+++ MethodUse.java
@@ -0,0 +1,15 @@
+
+public class MethodUse {
+
+	public static void main(String[] args) {
+		method myclass = new method();
+		myclass.method2(10);
+		int value = myclass.method3();
+		System.out.println("method3이 리턴한값: "+value);
+		int value2 = myclass.method5(20);
+		System.out.println("method5가 리턴한값: "+value2);
+		
+
+	}
+
+}
---
+++ method.java
@@ -0,0 +1,28 @@
+
+public class method {
+
+		//public 리턴타입 메소드명 (매개변수 들) { 구현 }
+		//여러가지 형태의 메소드
+		public void method1() {
+			System.out.println("m1이 실행됨");
+		}
+		
+		public void method2(int x) {
+			System.out.println(x +"을 이용한 m2 실행");
+		}
+		
+		public int method3() {
+			System.out.println("m3 실행");
+			return 10;
+		}
+		
+		public void method4(int x, int y) {
+			System.out.println(x + y +"m4가 실행");
+		}
+		
+		public int method5(int x) {
+			System.out.println("m5가 실행");
+			return x;
+		}
+}
+
